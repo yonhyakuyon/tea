e59ef1faf0196948a01f9a349498b2dbb1b98b0d
Dimitry <seventeenstabs@gmail.com>
2024-07-21 21:54:14 +0300

    Made .exe

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import time
 import winsound
-import os
 
 while True:
     s = 0  # iterible for seconds
     min = 0  # minutes
-    user_time = int(input("Введите время заварки: "))
+    user_time = int(input("Введите время заварки в секундах: "))
     for i in range(user_time):
         time.sleep(1)
         s += 1
         if s % 60 == 0:
             min += 1
-    winsound.Beep(666, 1000)
+    for i in range(6):
+        winsound.Beep(666, 700)
+        i += 1
