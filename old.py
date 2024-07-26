bfd78e562f25dfc58057b94a187885bfee7b2d1d
Dimitry Kriushenkov <seventeenstabs@gmail.com>
2024-07-26 21:00:42 +0300

    Invented GUI

--- old.py
+++ old.py
@@ -1,5 +1,6 @@
 import time
 import winsound
+import shelve
 import pyautogui
 import os
 from tkinter import *
@@ -21,13 +22,31 @@ def save_time(user_time):
         db.write(str(user_time))
 
 
+def save_sound(sound_path):
+    with shelve.open("settings") as db:
+        db["sound"] = sound_path
+
+
 def load_time():
     with open("settings.txt", "r") as db:
         return db.read()
 
 
+def load_sound():
+    with shelve.open("settings") as db:
+        return db["sound"]
+
+
 def set_up():
+    # main.destroy()
     get = pyautogui.prompt("Set up settings", "Set up settings", ["ok"])
+    # user_sound = input("Enter sound (y/n): ")
+    # if user_sound == "y":
+    #     print("I hope u move your .mp3 file to same folder as this program")
+    #     sound_path = input("Enter sound name: ")
+    #     save_sound(sound_path)
+    # else:
+    #     save_sound(0)
     save_time(get)
     print("Settings saved")
     timer()
@@ -35,11 +54,17 @@ def set_up():
 
 def start_message():
     TGREEN = "\033[32m"  # Green Text
-    print(TGREEN + "If u start program first time please set up settings")
-    print("Here will be timer :)")
-    ttk.Button(frm, text="Start timer", command=timer).grid(column=0, row=2)
-    ttk.Button(frm, text="Set up settings", command=set_up).grid(column=1, row=2)
-    input()  # need to run GUI
+    print(TGREEN + "Here will be timer :)")
+    # print("If u start program first time please set up settings")
+    # print("To set up settings print !s")
+    # print("To run timer print !t")
+    # user_mode = input()
+    # if user_mode == "!s":
+    #     set_up()
+    #     cls()
+    # elif user_mode == "!t":
+    #     timer()
+    #     cls()
 
 
 def timer():
